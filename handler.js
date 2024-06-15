be26ba4a6ff087a851957cfcaa691f44260be625
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-15 15:26:49 -0500

    configuracion dynamodb local

--- handler.js
+++ handler.js
@@ -1,13 +1,34 @@
+const aws = require("aws-sdk");
 
-const hello = async (event, context) => {
-    const segundos = new Date().getSeconds();
-    return {
-        "statusCode": 200,
-        "body": JSON.stringify({ 'message': "actualizada"+segundos.toString()})
-    }
+let dynamoDBClientParams={}
+
+
+const dynamodb = new aws.DynamoDB.DocumentClient(
+    {
+        region: 'localhost',
+        endpoint: 'http://localhost:8000',
+        accessKeyId: '',  // needed if you don't have aws credentials at all in env
+        secretAccessKey: '' // needed if you don't have aws credentials at all in env
+    }  
+);
+
+const getUsers = async (event, context) => {
+
+    const params = {
+        ExpressionAttributeValues: {':pk':'1'},
+        KeyConditionExpression: "pk = :pk",
+        TableName: "usersTable",
+    };
+
+    return dynamodb.query(params).promise().then(res=>{
+        console.log(res);
+        return {
+            "statusCode": 200,
+            "body": JSON.stringify({ 'user':  res })
+        }
+    });
 }
 
 module.exports = {
-    hello
+    getUsers
 }
- 
